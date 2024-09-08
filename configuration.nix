56270ae90efe4bea64a2506525149ce9f93b81b7
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-09-08 08:10:24 -0400

    bluetooth managers added configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -156,6 +156,12 @@ in
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 
+
+# Bluetooth
+bluez
+bluez-tools
+blueman
+
 # Terminal Apps
 alacritty
 vim
