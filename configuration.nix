b5a09d245f95d9fb8b63b3a6ee3019e5c731051b
Ibrahim Karaagac <ikaraagac@gmail.com>
2024-10-14 11:20:02 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -96,10 +96,12 @@ environment.variables = {
     '';
   };
 
+  # Default GUI Desktop Manager: xfce, gnome 
+  services.displayManager.defaultSession  = "gnome";
 
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # KDE
   #services.displayManager.sddm.enable = true;
@@ -107,13 +109,48 @@ environment.variables = {
 
   # XFCE =========================================
   services.xserver.desktopManager.xfce.enable  = true;
-  services.displayManager.defaultSession  = "xfce";
 
   #================================================
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
+  };
+
+  #==========Graphics Card - Nvidia=============
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   #=========Printer==============================
@@ -280,6 +317,7 @@ goldendict-ng # Dictionary app
 syncthing 
 feh
 vlc
+davinci-resolve
 
 # Postgresql
 postgresql
@@ -498,3 +536,4 @@ services = {
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
+
